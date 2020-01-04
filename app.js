25ebdbc3dc7952a18dc1fc0e8b0c1c68a3a42944
Jan-David Stärk <jandavid.staerk@gmail.com>
2020-01-04 16:08:02 +0100

    Moved combinded-data to pi-stats

--- app.js
+++ app.js
@@ -3,37 +3,38 @@ const osutils = require('./lib/osutils');
 const pMS = require('pretty-ms');
 const { exec } = require('child_process');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.static('./frontend/dist'))
+app.use(cors());
 
-// Return all relevant data in a short notation
-// Currently supports only pi stats like CPU, RAM, ...
-app.get('/api/v1/data-combined', async (req, res) => {
+app.get('/api/v1/pi-stats', async (req, res) => {
     const q = await Promise.all([
         osutils.freeCommand(),
         osutils.harddrive(),
         osutils.cpuUsage(),
     ]);
 
+    let memory = q[0];
+    if (memory) memory = memory + 'mb';
     let uptimeMS = pMS(((osutils.sysUptime() || 1000) * 1000), { colonNotation: true }).toString();
     let usedDiskPct = (q[1].used / q[1].total);
-    if (usedDiskPct) usedDiskPct = (usedDiskPct * 100).toFixed(2) + ' %';
+    if (usedDiskPct) usedDiskPct = (usedDiskPct * 100).toFixed(2) + '%';
     let cpu = q[2];
-    if (cpu) cpu = (cpu * 100).toFixed(2) + ' %'
+    if (cpu) cpu = (cpu * 100).toFixed(2) + '%'
     let loadAvg5 = osutils.loadavg(5);
-    if (loadAvg5) loadAvg5 = (loadAvg5 * 100).toFixed(2) + ' %';
+    if (loadAvg5) loadAvg5 = (loadAvg5 * 100).toFixed(2) + '%';
     let loadAvg15 = osutils.loadavg(15);
-    if (loadAvg15) loadAvg15 = (loadAvg15 * 100).toFixed(2) + ' %';
+    if (loadAvg15) loadAvg15 = (loadAvg15 * 100).toFixed(2) + '%';
 
-    data = {};
-    data.piStats = [];
-    data.piStats.push({ name: 'Uptime', value: uptimeMS || '0' });
-    data.piStats.push({ name: 'Mem', value: q[0] || '0' });
-    data.piStats.push({ name: 'CPU', value: cpu  || '0' });
-    data.piStats.push({ name: 'DU', value: usedDiskPct || '0' });
-    data.piStats.push({ name: 'Load 5m', value: loadAvg5 || '0' });
-    data.piStats.push({ name: 'Load 15m', value: loadAvg15 || '0' });
+    data = [];
+    data.push({ name: 'Uptime', value: uptimeMS || '0' });
+    data.push({ name: 'Mem', value: memory || '0' });
+    data.push({ name: 'CPU', value: cpu  || '0' });
+    data.push({ name: 'DU', value: usedDiskPct || '0' });
+    data.push({ name: 'Load 5m', value: loadAvg5 || '0' });
+    data.push({ name: 'Load 15m', value: loadAvg15 || '0' });
 
     res.json(data);
 });
